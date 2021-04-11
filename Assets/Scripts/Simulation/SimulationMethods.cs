eb490b017af5ad3eadb0f90581a0387703933e0e
Jess Nguyen <38814279+Jess-Nguy@users.noreply.github.com>
2021-04-10 20:13:12 -0400

    Revert "Merging Power source"

--- Assets/Scripts/Simulation/SimulationMethods.cs
+++ Assets/Scripts/Simulation/SimulationMethods.cs
@@ -6,26 +6,15 @@ using UnityEngine.SceneManagement;
 public class SimulationMethods : MonoBehaviour
 {
     public bool simulationActiveState = false;
-    /*
-    * When the user clicks on the Play button, play the simulation
-    */
-    public string currentSimulationMode = "ViewMode;
+    public string currentSimulationMode = "ViewMode";
     public void SimulationPlay()
     {
         simulationActiveState = true;
     }
-
-    /*
-    * When the user clicks on the Pause button, pause the simulation 
-    */
     public void SimulationStop()
     {
         simulationActiveState = false;
     }
-
-    /*
-    * When the user clicks on the Reset button, resets the scene to its original state 
-    */
     public void SimulationReset()
     {
         SceneManager.LoadScene("_Main_Scene");
