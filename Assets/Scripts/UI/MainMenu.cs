63360ef310b61e6086117b0d47644a2b3cda686b
MetalOath <arnaout.othman@gmail.com>
2021-05-04 23:33:28 -0400

    Added Main Menu

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+
+    public void QuitApp()
+    {
+        Application.Quit();
+    }
+    public void NewWorkspace()
+    {
+        SceneManager.LoadScene("_Main_Scene");
+    }
+    public void ExampleWorkspace()
+    {
+        SceneManager.LoadScene("_Example_Scene");
+    }
+}
